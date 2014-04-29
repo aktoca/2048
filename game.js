631ac1b926a78f5dd64188aaa032ee7c995a9a74
Steve Long <steven989@gmail.com>
2014-04-29 19:30:19 -0400

    added initalizing game with two randomly placed squares'

--- game.js
+++ game.js
@@ -2,12 +2,49 @@
 
 function Square() {
 
-    this.value = 2
-    this.x = 1
-    this.y = 1
+        this.value = 2
+        this.x = "1"
+        this.y = "1"
 
-}
+        var id
+
+        this.randLocation = function() {
 
 
-square = new Square()
+            while(true) {
+                this.x = Math.ceil(Math.random()*4)
+                this.y = Math.ceil(Math.random()*4)
+
+                id = this.x+""+this.y
+
+                if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
+            }
+        }
+
+        this.render = function() {
+
+            id = this.x+""+this.y
+
+            this.$me = $('<div class= "cell_content"></div>')
+
+            $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me)
+
+        }
+
+    }
+
+
+
+    $(function(){
+    
+    square = new Square()
+    square.randLocation()
+    square.render()
+
+    square2 = new Square()
+    square2.randLocation()
+    square2.render()
+
+    })
+
 
