60745f6ab1d7b823f8006b6d542c519f21a7cf92
Steve Long <steven989@gmail.com>
2014-05-02 18:27:36 -0400

    checked value against obstacle, merge if value is the same, kill the obstacle, update the javascript object value and the DOM value

--- game.js
+++ game.js
@@ -4,57 +4,58 @@ var children = [];
 function Square() {
 
   this.value = 2
-    this.x
-    this.y
+  this.x
+  this.y
 
-    var id
+  var id
 
-    this.randLocation = function() {
+  this.randLocation = function() {
 
-      while(true) {
-        this.x = Math.ceil(Math.random()*4)
-          this.y = Math.ceil(Math.random()*4)
-
-          id = this.x+""+this.y
-
-          if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
-      }
-    }
-
-  this.render = function() {
-
-    id = this.x+""+this.y
-
-      this.$me = $('<div class= "cell_content"></div>')
-
-      $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me)
-
-  }
-
-  this.moveRender = function(current_x,current_y) {
-
-    var current_parent_id = current_x+""+current_y
+    while(true) {
+      this.x = Math.ceil(Math.random()*4)
+      this.y = Math.ceil(Math.random()*4)
 
       id = this.x+""+this.y
 
-      var _this = $('.square_container').filter(function(){return $(this).attr('id') == current_parent_id}).children()
+      if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
+    }
+}
 
-      var anotherParent = $('.square_container').filter(function(){return $(this).attr('id') == id})
+this.render = function() {      // creates the square on the board for the first time
 
-      _this.appendTo(anotherParent)   
+  id = this.x+""+this.y
 
-  }
+  this.$me = $('<div class= "cell_content '+this.value+'">'+this.value+'</div>')
 
-  this.move = function(keyPressed) {
+  $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me)
 
-    var current_x = this.x
-      var current_y = this.y
+}
+
+this.moveRender = function(current_x,current_y) {       // moves the square around after it's been created
+
+  var current_parent_id = current_x+""+current_y
+
+  id = this.x+""+this.y
+
+  var _this = $('.square_container').filter(function(){return $(this).attr('id') == current_parent_id}).children()
+
+  var anotherParent = $('.square_container').filter(function(){return $(this).attr('id') == id})
+
+  _this.appendTo(anotherParent)   
+
+
+}
+
+this.move = function(keyPressed) {
+
+  var current_x = this.x
+  var current_y = this.y
 
       if (keyPressed == 37) { // left
 
         var parentsToLeft = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] < current_y && $(this).children().length > 0})
 
-          var parentsYCoordinates = [0];
+        var parentsYCoordinates = [0];
 
         parentsToLeft.each(function(){
 
@@ -64,10 +65,17 @@ function Square() {
 
         var maxLeftParentY = Math.max.apply(Math,parentsYCoordinates)
 
-        this.y = maxLeftParentY + 1
-
         var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxLeftParentY})
 
+        if (this.checkValue(obstacle[0])) {
+
+          this.y = maxLeftParentY
+
+        } else {
+
+          this.y = maxLeftParentY + 1
+
+        }
 
       }
 
@@ -76,7 +84,7 @@ function Square() {
 
         var parentsToRight = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] > current_y && $(this).children().length > 0})
 
-          var parentsYCoordinates = [5];
+        var parentsYCoordinates = [5];
 
 
         parentsToRight.each(function(){
@@ -85,22 +93,33 @@ function Square() {
 
         })
 
-        var maxRightParentY = Math.min.apply(Math,parentsYCoordinates)
-
-        this.y = maxRightParentY - 1
+        var maxRightParentY = Math.min.apply(Math,parentsYCoordinates)  
 
         var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxRightParentY})
 
+        if (this.checkValue(obstacle[0])) {
+
+          this.y = maxRightParentY
+
+        } else {
+
+          this.y = maxRightParentY - 1
+
+        }
+
       }
 
-    this.moveRender(current_x,current_y)
+      this.moveRender(current_x,current_y)  // move the square
 
-  }
+      this.kill(obstacle[0])  // kill the target if overlap occurs (i.e. value is the same)
+
+    }
 
 
-  this.moveUpDown = function(move) { 
+
+    this.moveUpDown = function(move) { 
     if (move == 0){return} //if row doesn't need to move -get out! 
-    var old_x = this.x
+      var old_x = this.x
     var old_y = this.y
     var newX = (old_x + move);
 
@@ -113,14 +132,51 @@ function Square() {
       if (move > 0 ){ 
         nextMove = move -1; 
       //moving up
-      } else {               
-        nextMove = move +1;
-      }
-      this.moveUpDown(nextMove);
+    } else {               
+      nextMove = move +1;
     }
+    this.moveUpDown(nextMove);
   }
-  
-  
+}
+
+
+
+    this.checkValue = function(squareToCheckAgainst) {
+
+      if (squareToCheckAgainst) {
+
+        return squareToCheckAgainst.value == this.value
+
+      }
+
+    }
+
+    this.kill = function(squareToKill) {
+
+      if (squareToKill && this.checkValue(squareToKill)) {
+
+        console.log(squareToKill)
+
+      var id = squareToKill.x+""+squareToKill.y
+
+      $('.square_container').filter(function(){return $(this).attr('id') == id}).children().eq(0).remove()   // Kill the square DOM on the game board
+
+      console.log( $('.square_container').filter(function(){return $(this).attr('id') == id}))
+
+      squares.splice($.inArray(squareToKill,squares),1)
+
+      this.value *= 2
+
+      $('.square_container').filter(function(){return $(this).attr('id') == id}).children().removeClass((this.value/2).toString()).addClass((this.value).toString())  // change the class of this square to the current value
+
+      $('.square_container').filter(function(){return $(this).attr('id') == id}).children().text(this.value)  // change the display value to the current value
+
+
+      }
+
+    }
+
+
 }
 
 
@@ -139,32 +195,32 @@ function massMove(event) {
 
           var object = squares.filter(function(square){return square.x == co_x && square.y == co_y})
 
-            if (object != 0) {children.push(object)}
+          if (object != 0) {children.push(object)}
 
         }
 
-      }
-
     }
 
-    else if (event.keyCode == 39) {
-
-      for(var co_x=1;co_x<=4;co_x++){
-
-        for(var co_y=4;co_y>=1;co_y--){
-
-          var object = squares.filter(function(square){return square.x == co_x && square.y == co_y})
-
-            if (object != 0) {children.push(object)}
-
-        }
-
-      }
-
-    }
-
-    children.forEach(function(square){square[0].move(event.keyCode)})
   }
+
+  else if (event.keyCode == 39) {
+
+    for(var co_x=1;co_x<=4;co_x++){
+
+      for(var co_y=4;co_y>=1;co_y--){
+
+        var object = squares.filter(function(square){return square.x == co_x && square.y == co_y})
+
+        if (object != 0) {children.push(object)}
+
+      }
+
+  }
+
+}
+
+children.forEach(function(square){square[0].move(event.keyCode)})
+}
 
   //find all cells  taken by objects per row
 
