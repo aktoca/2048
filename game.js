3de0ecaad4b057e642c8b11541923dd26460f3fa
aktoca <aktoca1@gmail.com>
2014-05-26 13:39:13 -0400

    cleaned up code

--- game.js
+++ game.js
@@ -2,115 +2,95 @@ var squares = [];
 var children = [];
 
 function Square() {
-
-  this.value = 2
-    this.x
-    this.y
-
-    var id
+  this.value = 2;
+    this.x;
+    this.y;
+    var id;
 
     this.randLocation = function() {
-
       while(true) {
-        this.x = Math.ceil(Math.random()*4)
-          this.y = Math.ceil(Math.random()*4)
-
-          id = this.x+""+this.y
-
+        this.x = Math.ceil(Math.random()*4);
+          this.y = Math.ceil(Math.random()*4);
+          id = this.x+""+this.y;
           if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
       }
     }
 
   this.render = function() {
-
-    id = this.x+""+this.y
-
-      this.$me = $('<div class= "cell_content _'+this.value+'">'+this.value+'</div>')
-
-      $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me)
+    id = this.x+""+this.y;
+      this.$me = $('<div class= "cell_content _'+this.value+'">'+this.value+'</div>');
+      $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me);
 
   }
 
   this.moveRender = function(current_x,current_y) {
-
     var current_parent_id = current_x+""+current_y;
-
     id = this.x+""+this.y;
-
     var _this = $('.square_container').filter(function(){return $(this).attr('id') == current_parent_id}).children();
-
     var anotherParent = $('.square_container').filter(function(){return $(this).attr('id') == id});
-
     _this.appendTo(anotherParent);
-
     return
-
   }
 
 
-
-
-
-
   this.move = function(keyPressed) {
-
     var current_x = this.x;
-      var current_y = this.y;
+    var current_y = this.y;
 
-      if (keyPressed == 37) { // left
+    if (keyPressed == 37) { // left
 
-        var parentsToLeft = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] < current_y && $(this).children().length > 0})
+      var parentsToLeft = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] < current_y && $(this).children().length > 0})
 
-          var parentsYCoordinates = [0];
+        var parentsYCoordinates = [0];
 
-        parentsToLeft.each(function(){
+      parentsToLeft.each(function(){
 
-          parentsYCoordinates.push($(this).attr('id')[1]);
+        parentsYCoordinates.push($(this).attr('id')[1]);
 
-        })
+      })
 
-        var maxLeftParentY = Math.max.apply(Math,parentsYCoordinates);
+      var maxLeftParentY = Math.max.apply(Math,parentsYCoordinates);
 
-          var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxLeftParentY})
+      var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxLeftParentY})
 
-          if (this.checkValue(obstacle[0])) {
+        if (this.checkValue(obstacle[0])) {
 
-            this.y = maxLeftParentY;
+          this.y = maxLeftParentY;
 
-          } else {
+        } else {
 
-            this.y = maxLeftParentY + 1;
+          this.y = maxLeftParentY + 1;
 
-          }
-      }
-
-
-      else if (keyPressed == 39) { // right
-
-        var parentsToRight = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] > current_y && $(this).children().length > 0})
-
-          var parentsYCoordinates = [5];
+        }
+    }
 
 
-        parentsToRight.each(function(){
+    else if (keyPressed == 39) { // right
 
-          parentsYCoordinates.push($(this).attr('id')[1])
+      var parentsToRight = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] > current_y && $(this).children().length > 0})
 
-        })
+        var parentsYCoordinates = [5];
 
-        var maxRightParentY = Math.min.apply(Math,parentsYCoordinates)  
 
-          var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxRightParentY})
+      parentsToRight.each(function(){
 
-          if (this.checkValue(obstacle[0])) {
+        parentsYCoordinates.push($(this).attr('id')[1])
 
-            this.y = maxRightParentY
+      })
 
-          } else {
+      var maxRightParentY = Math.min.apply(Math,parentsYCoordinates)  
 
-            this.y = maxRightParentY - 1
-          }
-      }
+        var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxRightParentY})
+
+        if (this.checkValue(obstacle[0])) {
+
+          this.y = maxRightParentY
+
+        } else {
+
+          this.y = maxRightParentY - 1
+        }
+    }
 
     this.moveRender(current_x,current_y)
 
@@ -122,26 +102,24 @@ function Square() {
     var old_x = this.x;
     var old_y = this.y;
     var newX = (old_x + move);
-    console.log(this.x);
-      // find a cell thats in the way of  a move to later match the value
-      var nextBlock =  $.grep(squares, function(e){ return e.y == old_y && e.x == newX; })
-      if (nextBlock) {
-        var block  =  nextBlock[0];
-      } 
+
+    // find the cell in the way of a move 
+    var nextBlock =  $.grep(squares, function(e){ return e.y == old_y && e.x == newX; });
+    if (nextBlock) {var block  =  nextBlock[0]; } 
 
     // if the nth most cell is empty or has the same value as 'this' ->  move there 
     if ($('#'+newX+""+old_y).is(':empty') || (this.checkValue(block) == true) )  {
       this.x = newX; 
       this.moveRender(old_x,old_y);
-      if (block != this){this.kill(block);}
-      return
+      if (block != this){this.kill(block);} // kill only when the block above is not self but has the same value
+      return;
     } else if (move == 0){   //if row doesn't need to move -get out! 
       return;
     } else {
-      // moving down by subtracking  
+      // move down by subtracking  
       if (move > 0 ){ 
         nextMove = move -1; 
-        //moving up by adding
+        //move up by adding
       } else {               
         nextMove = move +1;
       }
@@ -156,10 +134,8 @@ function Square() {
   this.checkValue = function(squareToCheckAgainst) {
     if (squareToCheckAgainst){
       if (squareToCheckAgainst.value == this.value) {
-        console.log(true);
         return true;
       } else {
-        console.log(false);
         return false;
       }
     }
@@ -248,6 +224,7 @@ function massMove(event) {
     moveRow(1, 3); // move dowe upto 3 rows
   }
 
+
   //takes each cell_container in a row and moves it to the next empty space up/down
   function moveRow (rows, moves){
     row = $.grep(squares, function(e){ return e.x == rows; });
@@ -263,30 +240,19 @@ function massMove(event) {
 
 
 
-
-
-
 function createSquare(num) {
-
-
   for(var i = 1; i <= num; i++) {
-
     square = new Square()
       squares.push(square)
       square.randLocation()
       square.render()
-
   }
-
-
 }
 
 
 
 $(function(){
-
   $(document).on('keydown', massMove);
   //start the game with two squares
   createSquare(2);
-
 });
