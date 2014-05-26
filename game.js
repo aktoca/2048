7c6528fc995d1965e79b601199daa39e2432c4f5
aktoca <aktoca1@gmail.com>
2014-05-26 14:54:52 -0400

    Converted leftright to same format as updown. Moves and combindes values, but next block is not acurate so it skips

--- game.js
+++ game.js
@@ -1,25 +1,24 @@
 var squares = [];
-var children = [];
 
 function Square() {
   this.value = 2;
-    this.x;
-    this.y;
-    var id;
+  this.x;
+  this.y;
+  var id;
 
-    this.randLocation = function() {
-      while(true) {
-        this.x = Math.ceil(Math.random()*4);
-          this.y = Math.ceil(Math.random()*4);
-          id = this.x+""+this.y;
-          if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
-      }
+  this.randLocation = function() {
+    while(true) {
+      this.x = Math.ceil(Math.random()*4);
+      this.y = Math.ceil(Math.random()*4);
+      id = this.x+""+this.y;
+      if($('.square_container').filter(function(){return $(this).attr('id') == id}).children().length == 0) {break}
     }
+  }
 
   this.render = function() {
     id = this.x+""+this.y;
-      this.$me = $('<div class= "cell_content _'+this.value+'">'+this.value+'</div>');
-      $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me);
+    this.$me = $('<div class= "cell_content _'+this.value+'">'+this.value+'</div>');
+    $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me);
 
   }
 
@@ -33,70 +32,37 @@ function Square() {
   }
 
 
-  this.move = function(keyPressed) {
-    var current_x = this.x;
-    var current_y = this.y;
 
-    if (keyPressed == 37) { // left
+  this.moveLeftRight = function(move) { 
+    var old_x = this.x;
+    var old_y = this.y;
+    var newY = (old_y + move);
 
-      var parentsToLeft = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] < current_y && $(this).children().length > 0})
+    // find the cell in the way of a move 
+    var nextBlock =  $.grep(squares, function(e){ return e.Y == newY && e.x == old_x; });
+    if (nextBlock) {var block  =  nextBlock[0]; } 
 
-        var parentsYCoordinates = [0];
-
-      parentsToLeft.each(function(){
-
-        parentsYCoordinates.push($(this).attr('id')[1]);
-
-      })
-
-      var maxLeftParentY = Math.max.apply(Math,parentsYCoordinates);
-
-      var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxLeftParentY})
-
-        if (this.checkValue(obstacle[0])) {
-
-          this.y = maxLeftParentY;
-
-        } else {
-
-          this.y = maxLeftParentY + 1;
-
-        }
+    // if the nth most cell is empty or has the same value as 'this' ->  move there 
+    if ($('#'+old_x+""+newY).is(':empty') || (this.checkValue(block) == true) )  {
+      this.y = newY; 
+      this.moveRender(old_x,old_y);
+      if (block != this){this.kill(block);} // kill only when the block above is not self but has the same value
+      return;
+    } else if (move == 0){   //if row doesn't need to move -get out! 
+      return;
+    } else {
+      // move down by subtracking  
+      if (move > 0 ){ 
+        nextMove = move -1; 
+        //move up by adding
+      } else {               
+        nextMove = move +1;
+      }
+      this.moveLeftRight(nextMove);
     }
-
-
-    else if (keyPressed == 39) { // right
-
-      var parentsToRight = $('.square_container').filter(function(){return $(this).attr('id')[0] == current_x && $(this).attr('id')[1] > current_y && $(this).children().length > 0})
-
-        var parentsYCoordinates = [5];
-
-
-      parentsToRight.each(function(){
-
-        parentsYCoordinates.push($(this).attr('id')[1])
-
-      })
-
-      var maxRightParentY = Math.min.apply(Math,parentsYCoordinates)  
-
-        var obstacle = squares.filter(function(square){return square.x == current_x && square.y == maxRightParentY})
-
-        if (this.checkValue(obstacle[0])) {
-
-          this.y = maxRightParentY
-
-        } else {
-
-          this.y = maxRightParentY - 1
-        }
-    }
-
-    this.moveRender(current_x,current_y)
-
-      this.kill(obstacle[0]);
-
   }
+
+
 
   this.moveUpDown = function(move) { 
     var old_x = this.x;
@@ -142,30 +108,18 @@ function Square() {
   }
 
   this.kill = function(squareToKill) {
-
     if (squareToKill && this.checkValue(squareToKill)) {
-
       var id = squareToKill.x+""+squareToKill.y
         // Kill the square DOM on the game board
         $('.square_container').filter(function(){return $(this).attr('id') == id}).children().eq(0).remove()   
-
         squares.splice($.inArray(squareToKill,squares),1)
-
-        children.splice($.inArray(squareToKill,Array.prototype.concat.apply([],children)),1)
-
         this.value *= 2
         // change the class of this square to the current value
         $('.square_container').filter(function(){return $(this).attr('id') == id}).children().removeClass("_"+(this.value/2).toString()).addClass("_"+(this.value).toString())  
         // change the display value to the current value
         $('.square_container').filter(function(){return $(this).attr('id') == id}).children().text(this.value) 
-
     }
-
   } 
-
-
-
-
 }
 
 
@@ -174,45 +128,19 @@ function Square() {
 
 function massMove(event) {
 
-  if (event.keyCode == 37 || event.keyCode == 39){
 
-    if (event.keyCode == 37) {
-
-      for(var co_x=1;co_x<=4;co_x++){
-
-        for(var co_y=1;co_y<=4;co_y++){
-
-          var object = squares.filter(function(square){return square.x == co_x && square.y == co_y})
-
-            if (object != 0) {children.push(object)}
-
-        }
-
-      }
-
-    }
-
-    else if (event.keyCode == 39) {
-
-      for(var co_x=1;co_x<=4;co_x++){
-
-        for(var co_y=4;co_y>=1;co_y--){
-
-          var object = squares.filter(function(square){return square.x == co_x && square.y == co_y})
-
-            if (object != 0) {children.push(object)}
-
-        }
-
-      }
-
-    }
-
-    children.forEach(function(square){square[0].move(event.keyCode)})
+  if (event.keyCode == 37) {
+    moveColumn(2, -1); // move column2 left  only once
+    moveColumn(3, -2); // move column3 left 2 rows max
+    moveColumn(4, -3); // move column4 left 3rows max
+  }
+  else if (event.keyCode == 39) {
+    moveColumn(3, 1); // move column3 right only one row 
+    moveColumn(2, 2); // move column2 right to 2 times
+    moveColumn(1, 3); // move column1 right to 3 rows
   }
 
   //find all cells  taken by objects per row
-
   if (event.keyCode == 38){ // arrow up
     moveRow(2, -1); // move row2 up only once
     moveRow(3, -2); // move row3 up 2 rows max
@@ -225,6 +153,14 @@ function massMove(event) {
   }
 
 
+  function moveColumn (columns, moves){
+    column = $.grep(squares, function(e){ return e.y == columns; });
+    $(column).each(function(i, square_container){ 
+      sq = this
+      sq.moveLeftRight(moves)
+    });
+  }
+
   //takes each cell_container in a row and moves it to the next empty space up/down
   function moveRow (rows, moves){
     row = $.grep(squares, function(e){ return e.x == rows; });
@@ -233,6 +169,7 @@ function massMove(event) {
       sq.moveUpDown(moves)
     });
   }
+
   if (event.keyCode >= 37 && event.keyCode <= 40){ 
     createSquare(1);
   }
