ab182655d34aa5613217ea72c2e7ceb176de9f32
aktoca <aktoca1@gmail.com>
2014-07-06 19:55:13 -0400

    Left/Up

--- 2048.js
+++ 2048.js
@@ -3,6 +3,7 @@ var squares = [];
 function Square() {
   this.value = 2;
   this.id;
+  var id = "#"+this.id;
 
   this.randLocation = function() {
     while(true){
@@ -10,11 +11,10 @@ function Square() {
       var y = Math.ceil(Math.random()*4);
       this.id = (x * 10 ) + y; 
       var id = "#" + this.id
-      //check to make sure not already taken
-      if($(id).children().length == 0 ){
-        break;
-      }
-
+        //check to make sure not already taken
+        if($(id).children().length == 0 ){
+          break;
+        }
     }
   }
 
@@ -22,7 +22,6 @@ function Square() {
     id = this.id.toString();
     this.$me = $('<div class= "cell_content _'+this.value+'">'+this.value+'</div>');
     $('.square_container').filter(function(){return $(this).attr('id') == id}).append(this.$me);
-
   }
 
   this.moveRender = function(currentSqrId){
@@ -42,10 +41,18 @@ function Square() {
     for (i =1; i <= limit;i++ ){
       var nextLine =  $.grep(squares, function(e){ return e.id == old_id + direction; });
       var next = nextLine[0];
-      console.log('next sqr: '+nextLine);
       if (next){
-        console.log('check value and if eql move& kill');
+        //Check value of next square and if == to this, kill
+        if (this.checkValue(next)){
+          this.id = old_id + direction;
+          this.moveRender(old_id);
+          this.merge(next);
+          return;
+        }else{
+          console.log(this.id + ' is no match,dont move. try next square');
+        }
       } else {
+        //if next sqr is empty move there
         console.log(this.id);
         this.id = old_id + direction;
         console.log(this);
@@ -55,6 +62,36 @@ function Square() {
     }
   }
 
+  this.checkValue = function(squareToCheckAgainst) {
+    if (squareToCheckAgainst){
+      if (squareToCheckAgainst.value == this.value) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  this.merge = function(squareToKill) {
+    newId = "#"+this.id;
+    // ($('#'+squareToKill.id).children().length > 1)
+      if (squareToKill && this.checkValue(squareToKill)) {
+
+      // Kill the square DOM on the game board
+      //$("#"+squareToKill.id).children().eq(0).remove();
+      // Remove from array squares
+      squares.splice($.inArray(squareToKill,squares),1);
+
+      this.value *= 2;
+
+      // change the class of this square to the current value
+      $(newId).children().removeClass("_"+(this.value/2).toString()).addClass("_"+(this.value).toString());
+      // change the display value to the current value
+      $(newId).children().text(this.value);
+
+      console.log("id to change:" +newId);
+    }
+  }  
 }
 
 function massMove(event){
@@ -65,17 +102,31 @@ function massMove(event){
       line = $.grep(squares, function(e){ return Math.floor(e.id / 10) == i; });
       $(line).each(function(w, square_container){ 
         this.move(-10, i-1);
+        alert(i);
       });
     }
+    for (x in squares){ 
+      if($("#"+squares[x].id).children().length > 1) {
+        console.log('delete');
+        $("#"+squares[x].id).children().eq(0).remove();
+      }
+    }
+
   } else if (event.keyCode == 38){
     console.log("up");
     for (var i=3; i>=1; i--){
       line = $.grep(squares, function(e){ return Math.floor(e.id % 10) == i; });
-      console.log(line);
       $(line).each(function(w, square_container){ 
         this.move(1, 4-i );
       });
     }
+    for (x in squares){ 
+      if($("#"+squares[x].id).children().length > 1) {
+        console.log('delete');
+        $("#"+squares[x].id).children().eq(0).remove();
+      }
+    }
+
   } else if (event.keyCode == 39){
     console.log("right");
   } else if (event.keyCode == 40){
@@ -84,9 +135,6 @@ function massMove(event){
 
 }
 
-function nextEmpty(direction){
-
-}
 
 function createSquare(num) {
   for(var i = 1; i <= num; i++) {
